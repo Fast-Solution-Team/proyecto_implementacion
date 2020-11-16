f45af2094a82433e1bf4943fb24f809396e49828
alPao29 <60638511+alPao29@users.noreply.github.com>
2020-11-15 18:52:33 -0600

    mostrar saldo en navigation drop

---
+++ app/Models/SaldoBilletera.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SaldoBilletera extends Model
+{
+    use HasFactory;
+
+    protected $table = 'saldo_billetera';
+    protected $fillable = [
+        'SALDO_BILLETERA'
+    ];
+}
+
--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -102,7 +102,7 @@
                     </x-slot>
                 </x-jet-dropdown>
                 <div class="block px-4 py-2 font-medium text-gray-500">
-                    {{'Saldo: 0.0'}}
+                    {{Auth::user()->getSaldoUser()}}
                 </div>
             </div>
 
