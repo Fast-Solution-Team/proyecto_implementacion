4fecf2ad9af5fcf6658990175f411a4b782b78bb
alPao29 <60638511+alPao29@users.noreply.github.com>
2020-12-03 14:10:11 -0600

    creacion de los modelos movmiento y transaccion

---
+++ app/Models/Movimiento.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movimiento extends Model
+{
+    protected $table='movimientos_billeteras';
+    use HasFactory;
+
+    protected $fillable = [
+        'ID_MOVIMIENTO', 'FECHA_MOVIMIENTO', 'ID_TRANSACCION', 'MONTO_TRANSACCION', 'SALDO_ANTERIOR', 'SALDO_POSTERIOR'
+        , 'USU_CRE', 'FEC_CRE', 'USU_MOD', 'FEC_MOD',
+    ];
+}
