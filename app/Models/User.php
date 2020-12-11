ac9739e52f1c5c028cc49ba5885cabfdd40ad7ec
JocsSan <60682833+JocsSan@users.noreply.github.com>
2020-12-11 17:43:22 -0600

    trabajo en el equema de roles y permisos con cmabios en el CreateNewUser.php, Kernel.php, el modelo User.php, en el app.php, la adicion de permission.php y PermissionsSeeder.php, la migracion de 2020_11_29_033540_create_permission_tables.php, y en seeders

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
@@ -19,6 +20,7 @@ class User extends Authenticatable
     use HasTeams;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -71,9 +73,9 @@ class User extends Authenticatable
 
         $depositos = DB::select('SELECT * FROM emoney.transacciones where ID_BILLETERA = ? and TIPO_TRANSACCION = "DP"', [ $this->id_billetera]);
         $conteo_deposito = count($depositos);
-      
+
         return $conteo_deposito;
     }
 
-    
+
 }
---
+++ database/seeders/PermissionsSeeder.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class PermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $now = new \DateTime();
+//--------------------------------------------------------------------------------------
+        $billetera_admin = DB::table('billeteras')->insert([
+            'ID_BILLETERA'=>'0000000000000000',
+            'FECHA_CREACION'=>$now->format('Y-m-d H:i:s'),
+            'BILLETERA_ASIGNADA' => "S",
+            'USU_CRE' => 'YO',
+            'FEC_CRE' => $now->format('Y-m-d H:i:s'),
+        ]);
+        //creacion de super admin
+        $super_admin_user = User::create([
+        'name' => 'admin',
+        'second_name' => 'second_name',
+        'lastname' => 'lastname',
+        'second_lastname' => 'second_lastname',
+        'fec_nac' => $now->format('Y-m-d H:i:s'),
+        'identidad' => '0000000000000',
+        'sexo' => 'm',
+        'email' => 'user.admin@email.com',
+        'id_billetera' => '0000000000000000',
+        'direccion' => 'direccion',
+        'estado_cliente' => "a",
+        'password' => Hash::make('user.password'),
+    ]);
+
+//        User::create(['name' => 'test', 'email' => 'test@gmail.com', 'password' => Hash::make('admin')]);
+
+        $role = Role::create(['name' => 'super_admin']);
+        $permission_array = [];
+        array_push($permission_array, Permission::create(['name' => 'transacciones']));
+        array_push($permission_array, Permission::create(['name' => 'ver_movientos']));
+
+        array_push($permission_array, Permission::create(['name' => 'ver_retiro']));
+        array_push($permission_array, Permission::create(['name' => 'ver_deposito']));
+        array_push($permission_array, Permission::create(['name' => 'ver_pagoServicio']));
+        array_push($permission_array, Permission::create(['name' => 'ver_enviosDinero']));
+        array_push($permission_array, Permission::create(['name' => 'depositos']));
+        array_push($permission_array, Permission::create(['name' => 'retiros']));
+
+        array_push($permission_array, Permission::create(['name' => 'create_users']));
+        array_push($permission_array, Permission::create(['name' => 'edit_users']));
+        array_push($permission_array, Permission::create(['name' => 'delete_users']));
+        array_push($permission_array, Permission::create(['name' => 'view_users']));
+
+        array_push($permission_array, Permission::create(['name' => 'create_roles']));
+        array_push($permission_array, Permission::create(['name' => 'edit_roles']));
+        array_push($permission_array, Permission::create(['name' => 'delete_roles']));
+        array_push($permission_array, Permission::create(['name' => 'view_roles']));
+        $super_admin_user->assignRole($role->id);
+        $role->syncPermissions($permission_array);
+    }
+}
