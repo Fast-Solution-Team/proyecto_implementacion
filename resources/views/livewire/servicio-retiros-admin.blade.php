c3cb36d647d8bae6bb0d94c9a2684abd1a4411a1
gisserodriguez <73208296+gisserodriguez@users.noreply.github.com>
2020-12-13 12:44:40 -0600

    etiqueta

---
+++ resources/views/livewire/servicio-retiros-admin.blade.php
@@ -0,0 +1,70 @@
+<div>
+    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day --}}
+
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+        <title>RETIROS</title>
+    </head>
+
+    <body>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+<div align="center" style="align-content: center">
+
+    <h3> RETIROS </h3>
+</div>
+
+    <form action="#" class="credit-card-div">
+        <div class="panel panel-default" >
+            <div class="panel-heading">
+
+
+                <div class="row ">
+                    <div class="col-md-12">
+                        <input type="text" class="form-control" placeholder="Nombre" />
+                    </div>
+                </div>
+
+                <div class="row ">
+                    <div class="col-md-12">
+                        <input type="text" class="form-control" placeholder="Billetera origen" />
+                    </div>
+                </div>
+
+                <div class="row ">
+                    <div class="col-md-12 pad-adjust">
+
+                        <input type="text" class="form-control" placeholder="Monto a retirar" />
+                    </div>
+                </div>
+
+
+                <div class="row ">
+                    <div class="col-md-6 col-sm-6 col-xs-6 pad-adjust">
+                        <input type="submit"  class="btn btn-primary btn-block" value="RETIRAR" />
+                    </div>
+                    <div class="col-md-6 col-sm-6 col-xs-6 pad-adjust">
+                        <input type="submit"  class="btn btn-primary btn-block" value="CANCELAR" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </form>
+
+    </body>
+    </html>
+
+
+</div>
+
