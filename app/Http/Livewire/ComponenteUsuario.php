71ad6c056d00fd1dd45bd03af1b81c7a60250d79
JocsSan <60682833+JocsSan@users.noreply.github.com>
2020-12-16 03:37:25 -0600

    Cambios en registro CreateNewUser.php y trabajo en edit-User.blade.php y ComponenteUsuario.php

--- app/Http/Livewire/ComponenteUsuario.php
+++ app/Http/Livewire/ComponenteUsuario.php
@@ -8,17 +8,23 @@ use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Livewire\WithPagination;
 use Spatie\Permission\Traits\HasRoles;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class ComponenteUsuario extends Component
 {
     public $search;
     public $users;
-    public $name, $email, $selected_id, $estado_cliente;
+    public $name, $email, $selected_id, $estado_cliente, $rol_asign, $rol;
     use WithPagination;
     public $updateMode = false;
+    protected $rules = [
+        'rol' => 'required'
+    ];
     public function render()
     {
         $users = User::paginate(10);
+//        $all_roles = Role::all()->pluck('name');
         return view('livewire.componente-usuario')->with('usuarios', $users);
     }
     private function resetInput()
@@ -34,6 +40,7 @@ class ComponenteUsuario extends Component
         $this->estado_cliente = $record->estado_cliente;
         $this->name = $record->name;
         $this->email = $record->email;
+        $this->rol_asign = $record->getRoleNames()->toArray();
         $this->updateMode = true;
     }
     public function update()
@@ -43,7 +50,8 @@ class ComponenteUsuario extends Component
             'selected_id' => 'required|numeric',
             'name' => 'required|min:5',
             'estado_cliente' => 'required|min:1|max:1',
-            'email' => 'required|email:rfc,dns'
+            'email' => 'required|email:rfc,dns',
+            'rol' => 'required'
         ]);
         if ($this->selected_id) {
             $record = User::find($this->selected_id);
@@ -54,6 +62,7 @@ class ComponenteUsuario extends Component
                 'usu_mod' => Auth::user()->name,
                 'fec_mod'=> $now->format('Y-m-d H:i:s')
             ]);
+            $record->assignRole($this->rol);
             $this->resetInput();
             $this->updateMode = false;
         }
--- resources/views/livewire/edit-User.blade.php
+++ resources/views/livewire/edit-User.blade.php
@@ -22,8 +22,33 @@
             <span class="text-red-500 text-xs italic py-1">{{ $message }}</span>
             @enderror
         </div>
+        <div class="w-auto px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="rol_asign">Rol:</label>
+            <input wire:model="rol_asign" type="text" class="appearance-none block w-full bg-gray-200 text-gray-700 border {{ $errors->has('rol_asign') ? ' border-red-500' : 'border-gray-200' }} rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" disabled>
+            @error('rol_asign')
+            <span class="text-red-500 text-xs italic py-1">{{ $message }}</span>
+            @enderror
+        </div>
+        <div class="w-auto px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="rol">Rol:</label>
+{{--            <select wire:model="rol" id="rol" class="appearance-none block w-full bg-gray-200 text-gray-700 borde">--}}
+{{--                <option selected="selected" disabled>{{"Seleccione el Servicio"}}</option>--}}
+{{--                <option value="super_admin">Super Admin</option>--}}
+{{--                <option value="moderador">Moderador</option>--}}
+{{--                <option value="usuario">Usuario</option>--}}
+{{--            </select>--}}
+            <select id="rol" wire:model="rol;"
+                             class="browser-default shadow appearance-none border rounded focus:border-blue-400 w-3/4 ml-5 mt-2 py-2 px-3 text-gray-700 mt-1 leading-tight"
+                             name="servicio">
+                <option value="" disabled>{{"Seleccione un rol"}}</option>
+                <option value="super_admin">super_admin</option>
+                <option value="super_admin">super_admin</option>
+                <option value="super_admin">super_admin</option>
+                <option value="super_admin">super_admin</option>
+            </select>
+        </div>
         <div class="w-auto pl-3 text-right">
-            <button wire:click="update()" class="px-3 py-2 bg-orange-200 text-orange-500 hover:bg-orange-500 hover:text-orange-100 rounded">Actualizar contacto</button>
+            <button wire:click="update()" class="px-3 py-2 bg-orange-200 text-orange-500 hover:bg-orange-500 hover:text-orange-100 rounded">Actualizar Usuario</button>
         </div>
     </div>
 </div>
